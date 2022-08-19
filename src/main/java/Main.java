f6ade8595125519b09a8819e192c83b755ba6a62
Akshay Narayan <okkhoy@gmail.com>
2022-08-19 11:25:27 +0800

    Track loans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
-    public static void main(String[] args) {
-        
+    static int[] loans = new int[100];
+    static int loansCount = 0;
+
+    public static void addLoan(int amount){
+        loans[loansCount] = amount;
+        loansCount++;
     }
+
+    public static void listLoans(){
+        for (int i = 0; i < loansCount; i++){
+            System.out.println(loans[i]);
+        }
+    }
+
+    public static void main(String[] args){
+        addLoan(10);
+        addLoan(20);
+        listLoans();
+    }
+
 }
